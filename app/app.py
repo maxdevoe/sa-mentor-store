2825a5f268c739134f5c9c20d4b0751692864441
Max Devoe <maxwell.devoe@klaviyo.com>
2022-09-27 12:18:53 -0400

    updates to dockerfile and catalog route

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return {"Hello": "Flask"}
+
+@app.route("/catalog")
+def catalog():
+    catalog = requests.get('https://raw.githubusercontent.com/benjimelito/Sample-Repository/master/sample3.json')
+    return catalog.json()
 
 if __name__ == "__main__":
 	app.run(debug=True,host="0.0.0.0")
